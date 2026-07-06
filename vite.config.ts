e6f751e19304a1861decfe4a231a0cc18a2b0d71
Timo <timo.vanderven04@gmail.com>
2026-07-06 19:18:17 +0200

    feat: support github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-		allowedHosts: true
-	},
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    server: {
+      allowedHosts: true
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+  }
 })
