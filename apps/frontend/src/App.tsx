325989c684e4ff1495f37594f2940f690ca7399d
Timo <timo.vanderven04@gmail.com>
2026-07-07 20:44:19 +0200

    fix(Cart): add cartprovider wrapper

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -12,28 +12,33 @@ import { CatalogPage } from './pages/CatalogPage'
 import { BrandsPage } from './pages/BrandsPage'
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage'
 import { ProfilePage } from './pages/ProfilePage'
+import { CartPage } from './pages/CartPage'
 import { AuthProvider } from './hooks/useAuth'
+import { CartProvider } from './hooks/useCart'
 
 function App() {
   return (
     <AuthProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:category" element={<CategoryPage />} />
-          <Route path="/catalog/:category/:product" element={<ProductPage />} />
-          <Route path="/merken" element={<BrandsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile/:view?" element={<ProfilePage />} />
-          <Route path="/over-ons" element={<AboutPage />} />
-          <Route path="/bezorging" element={<DeliveryInformationPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </HashRouter>
+      <CartProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:category" element={<CategoryPage />} />
+            <Route path="/catalog/:category/:product" element={<ProductPage />} />
+            <Route path="/merken" element={<BrandsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile/:view?" element={<ProfilePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/over-ons" element={<AboutPage />} />
+            <Route path="/bezorging" element={<DeliveryInformationPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </HashRouter>
+      </CartProvider>
     </AuthProvider>
   )
 }
